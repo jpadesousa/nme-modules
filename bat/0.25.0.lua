98c598987666f5b42f9eda179b25d050f843ffa5
jpadesousa <jpadesousa@gmail.com>
2025-04-23 15:22:31 +0200

    Added bat

---
+++ bat/0.25.0.lua
@@ -0,0 +1,10 @@
+-- -*- lua -*-
+--
+--
+--
+whatis([[Name : bat]])
+whatis([[Version : 0.25.0]])
+help([[A cat(1) clone with syntax highlighting and Git integration.]])
+
+-- Add to PATH
+prepend_path("PATH", "/cluster/work/nme/software/apps/bat/0.25.0")
